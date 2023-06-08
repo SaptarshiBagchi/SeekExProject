518376331730e8b5fced123b03a93c8f372ba88a
Saptarshi <roni.bagchi@gmail.com>
2023-06-08 16:45:34 +0530

    Dev - removed db string from repo

--- db.js
+++ db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 
 async function connectDb() {
   try {
-    await mongoose.connect('')
+    await mongoose.connect('<insert_db_string_here>')
   } catch (e) {
     console.error(e)
   }
 }
 
 module.exports = connectDb
+D
